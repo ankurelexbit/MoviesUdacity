b90aab01eaf9f98637cdbf5a5bd1265691badb67
ankur <ankur.elex.bit@gmail.com>
2015-09-23 00:57:11 +0530

    Basic functionality done

--- app/src/main/java/com/blogspot/androidapollo/movies/utilities/Constants.java
+++ app/src/main/java/com/blogspot/androidapollo/movies/utilities/Constants.java
@@ -6,5 +6,5 @@ package com.blogspot.androidapollo.movies.utilities;
 public class Constants {
 
     public static final String MOVIESDB_BASE_URL = "http://api.themoviedb.org";
-    public static final String MOVIESDB_KEY = "-----";
+    public static final String MOVIESDB_KEY = "72836f66ebc20d8a560d3f89cb925b3d";
 }
